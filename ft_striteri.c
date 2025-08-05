fbb74abc3c8e4c6f9480e56aae19b0db0979a270
PEDRO <pedrofuzaitti@gmail.com>
2025-08-05 17:09:32 -0300

    norma

--- ft_striteri.c
+++ ft_striteri.c
@@ -1,16 +1,15 @@
 #include "libft.h"
 
-
-void ft_striteri(char *s, void (*f)(unsigned int,char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-    size_t      i;
+	size_t      i;
 
-    i = 0;
-    if (!s || !f)
-        return;
-    while (s[i] != '\0')
-    {
-        f(i,s+i);
-        i++;
-    }
+	i = 0;
+	if (!s || !f)
+		return;
+	while (s[i] != '\0')
+	{
+		f(i,s+i);
+		i++;
+	}
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,25 +1,24 @@
 #include "libft.h"
 
-
-char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-  char *result;
-  size_t i;
-  size_t size;
-  
-  i = 0;
-  size = 0;
-  if (!s || !f)
-    return (0);
-  size = ft_strlen(s);
-  result = malloc(size + 1);
-  if (!result)
-    return (0);
-  while (s[i] != '\0')
-  {
-    result[i] =  f(i,s[i]);
-    i++;
-  }
-  result[i] = '\0';
-  return result;
+	char		*result;
+	size_t		i;
+	size_t		size;
+
+	i = 0;
+	size = 0;
+	if (!s || !f)
+		return (0);
+	size = ft_strlen(s);
+	result = malloc(size + 1);
+	if (!result)
+		return (0);
+	while (s[i] != '\0')
+	{
+		result[i] = f(i, s[i]);
+		i++;
+	}
+	result[i] = '\0';
+	return (result);
 }
